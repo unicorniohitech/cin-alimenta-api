d7bbc04697fa8e68c79d57bb9d9a9b7e8620d534
fvasconcelos23 <128514563+fvasconcelos23@users.noreply.github.com>
2023-03-31 00:47:27 -0300

    "tabela interagida com o banco de dados"

--- database/migrations/1680224502491_users.ts
+++ database/migrations/1680224502491_users.ts
@@ -3,10 +3,11 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'users'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-
+      table.increments()
+      table.string('username: ').notNullable()
+      table.string('email: ').notNullable()
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
@@ -15,7 +16,7 @@ export default class extends BaseSchema {
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
