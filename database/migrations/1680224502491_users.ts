ad8ff85656400fa417024c6fd4770b27856d2517
fvasconcelos23 <128514563+fvasconcelos23@users.noreply.github.com>
2023-03-31 01:27:55 -0300

    modificação da tabela user

--- database/migrations/1680224502491_users.ts
+++ database/migrations/1680224502491_users.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments()
+      table.increments('id')
       table.string('name').notNullable()
       table.string('username').notNullable().unique()
       table.string('email').notNullable().unique()
